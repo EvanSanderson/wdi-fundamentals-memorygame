6fd4137fc646f87e7fb4c647a055e64cfe983dfd
Evan <evan.j.sanderson@gmail.com>
2016-06-15 19:02:15 -0400

    added DOM manip elements

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,32 @@
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
 
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-}
+var gameBoard = document.getElementById('game-board');
+
+/*for (i=1;i<=4; ++i) {
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	document.getElementById('card')[i].appendChild('board');
+};*/
+
+var createBoard = function() {
+	for (i=0; i<=3; ++i) {
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	document.getElementByClassName('card').appendChild('board');
+	/* above line could also read newCard.appendChild('board')*/
+	};
+};
+
+createBoard();
+
+
+
+
+/*if (cardOne === cardTwo) {
+	alert('You found a match!');
+}*/
